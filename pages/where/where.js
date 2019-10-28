8260a1aad7659e6a5c0037a7687732a0f0007589
YCLlong <251121753@qq.com>
2019-10-28 17:26:09 +0800

    Initial Commit

---
+++ pages/where/where.js
@@ -0,0 +1,78 @@
+// pages/where.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    latitude:null,
+    longitude:null
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.getLocation({
+      type: 'wgs84',
+      success: (res) => {
+        debugger;
+        this.setData({
+           latitude:res.latitude, // 纬度
+           longitude:res.longitude // 经度
+        });
+      
+      }
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
